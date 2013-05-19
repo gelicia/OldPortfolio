0f85c698fcdf3f8dc369001a65547fa53abe46ff
gelicia <kdurivage@gelicia.com>
2013-05-19 09:07:32 -0500

    change text, colors, draw

--- js/main.js
+++ js/main.js
@@ -23,24 +23,28 @@ function dataLoad(){
 
   ds.fetch({ 
     success : function() {
+      //put each row of the google doc in an array of objects
       this.each(function(row){ data.push(row); });
-      //add a full date attribute for logic but keep the other one for display
-      data = data.map(function(d){d.fullDate = dateParse(d.date); return d;});
+      //nest the array with the date as a key
+      data = d3.nest()
+        .key(function(d) {return d.date;})
+        .entries(data);
+      //format the data date key to be a real date for the timescale
+      data = data.map(function(d){d.fullDate = dateParse(d.key); return d;});
       def.resolve();
     },
     error : function() {
       console.log("Error loading the spreadsheet");
       def.resolve();
     }
-  });
-
+  });  
   return def.promise();
 }
 
 function drawTimeline(){
   var svg = d3.select('svg');
   //this is the margin for the line of the bar chart. All labels need to fit above it
-  var margin = {top: 68, left: 20, right: 20};
+  var margin = {top: 50, left: 20, right: 20};
   var screenWidth = parseInt(svg.style("width"), 10);
 
   //min and max dates for the timeline
@@ -73,46 +77,49 @@ function drawTimeline(){
   //Draw month and year labels for the timeline
   svg.append("g")
   .attr({
-      "transform": "translate(0," + (margin.top - 35) + ")",
+      "transform": "translate(0," + (margin.top - 30) + ")",
       "class": "monthScale"
     })
     .call(monthAxis); 
-
   svg.append("g")
   .attr({
-      "transform": "translate(0," + (margin.top - 60) + ")",
+      "transform": "translate(0," + (margin.top - 50) + ")",
       "class": "yearScale"
     })
     .call(yearAxis); 
+
+  var circles = svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr({
+      cx: function(d){return timeScale(d.fullDate);},
+      cy: margin.top,
+      r: 7,
+      stroke: "black",
+      fill: "#46594B",
+      id: function(d){return "pt" + (d.key).replace('/', '');}
+    })
+    .on("click", function(d) { return displayInfo(d.key); })
+    .on("mouseover", function(d,i) {
+      d3.select("#pt" + (d.key).replace('/', ''))
+        .attr("r","10");
+    })
+    .on("mouseout", function(d,i) {
+      d3.select("#pt" + (d.key).replace('/', ''))
+        .attr("r","7");
+    });
+
+    /*circles.append('title')
+    .text(function(d, i){ return dataRaw[i].date + " - " + d.name;});*/
+}
+
+function displayInfo(date){
+
 }
 
 /*
-var circles = svg.selectAll('circle')
-.data(data).
-enter()
-.append("circle")
-.attr({
-  cx: function(d){return timeScale(d.date);},
-  cy: margin.top,
-  r: 7,
-  stroke: "black",
-  fill: "steelblue",
-  id: function(d,i){return "pt" + (dataRaw[i].date).replace('/', '');}
-})
-.on("click", function(d,i) { return displayInfo(d.date); })
-.on("mouseover", function(d,i) {
-  d3.select("#pt" + (dataRaw[i].date).replace('/', ''))
-  .attr("r","10");
-})
-.on("mouseout", function(d,i) {
-  d3.select("#pt" + (dataRaw[i].date).replace('/', ''))
-  .attr("r","7");
-});
 
-circles.append('title')
-.text(function(d, i){ return dataRaw[i].date + " - " + d.name;});
-
-function displayInfo(date){
 
   var infoGroup = svg.select("g#infoGroup");
   var rect;
@@ -161,5 +168,4 @@ function displayInfo(date){
     });
     
     lastDate = date;
-  }
-}}*/
+  }*/
